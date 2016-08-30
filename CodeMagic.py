9af4dd2ffe7005273e5f57d0af1eda570013e94f
liangxun1 <13302337816@189.cn>
2016-08-30 18:16:19 +0800

    finish url encode

--- CodeMagic.py
+++ CodeMagic.py
@@ -1,38 +1,77 @@
 # -*- coding: utf-8 -*-
 
-from tkinter import ttk
-from tkinter import *
-from tkinter.ttk import *
 import tkinter
+import tkinter.scrolledtext as tkst
+import urllib
+import urllib.parse
+from tkinter import *
+from tkinter import ttk
+
 
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self,master)
+        self.pack()
         self.grid(sticky='nswe')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         # 新建两个TAB页
         self.nRoot = ttk.Notebook(self)
-        BuilderFrame = ttk.Frame(self.nRoot)
-        WatchLog = ttk.Frame(self.nRoot)
-        self.nRoot.add(BuilderFrame, text='1转多')
-        self.nRoot.add(WatchLog, text='1转1')
+        one2N_Frame = ttk.Frame(self.nRoot)
+        one2one_Frame = ttk.Frame(self.nRoot)
+        self.nRoot.add(one2N_Frame, text='1转多')
+        self.nRoot.add(one2one_Frame, text='1转1')
         self.nRoot.rowconfigure(0, weight=1)
         self.nRoot.columnconfigure(0, weight=1)
         self.nRoot.grid(row=0, column=0, sticky='nswe', ipady=3, ipadx=3)
-        BuilderFrame.rowconfigure(0, weight=1)
-        BuilderFrame.columnconfigure(0, weight=1)
-        WatchLog.rowconfigure(0, weight=1)
-        WatchLog.columnconfigure(0, weight=1)
+        one2N_Frame.rowconfigure(0, weight=1)
+        one2N_Frame.columnconfigure(0, weight=1)
+        one2one_Frame.rowconfigure(0, weight=1)
+        one2one_Frame.columnconfigure(0, weight=1)
         # 第一个TAB页
-
+        # 原始文本
+        ttk.Label(one2N_Frame, text='原始文本：').grid(sticky=E)
+        self.oriText = tkst.ScrolledText(one2N_Frame, height=5)
+        self.oriText.grid(row=0, column=1)
+        ttk.Button(one2N_Frame, text='给我转', command=self.runTrans).grid(row=0, column=2, padx=15)
+        # URL编码
+        ttk.Label(one2N_Frame, text='URL编码：').grid(sticky=E)
+        self.urlText = tkst.ScrolledText(one2N_Frame, height=5)
+        self.urlText.grid(row=1, column=1)
+        # 16进制编码
+        ttk.Label(one2N_Frame, text='16进制编码：').grid(sticky=E)
+        self.hexText = tkst.ScrolledText(one2N_Frame, height=5)
+        self.hexText.grid(row=2, column=1)
+        # ASCII编码
+        ttk.Label(one2N_Frame, text='ASCII编码：').grid(sticky=E)
+        self.asciiText = tkst.ScrolledText(one2N_Frame, height=5)
+        self.asciiText.grid(row=3, column=1)
+        # MD5_16编码
+        ttk.Label(one2N_Frame, text='MD5_16编码：').grid(sticky=E)
+        self.md5_16Text = tkst.ScrolledText(one2N_Frame, height=5)
+        self.md5_16Text.grid(row=4, column=1)
+        # MD5_32编码
+        ttk.Label(one2N_Frame, text='MD5_32编码：').grid(sticky=E)
+        self.md5_32Text = tkst.ScrolledText(one2N_Frame, height=5)
+        self.md5_32Text.grid(row=5, column=1)
+        # BASE64编码
+        ttk.Label(one2N_Frame, text='BASE64编码：').grid(sticky=E)
+        self.base64Text = tkst.ScrolledText(one2N_Frame, height=5)
+        self.base64Text.grid(row=6, column=1)
 
 
         # 第二个TAB页
-#End Class -------------------------------------------------------------------------------------------------------------
+
+    # 消息处理函数
+    # 1toN转换
+    def runTrans(self):
+        s_oriText = self.oriText.get(1.0, END).lstrip().rstrip()
+        # url编码
+        self.urlText.delete(1.0, END)
+        self.urlText.insert(END, urllib.parse.quote_plus(s_oriText))
 
 
-    # Targets:
+# End Class Application--------------------------------------------------------------------------------------------------
 
 def main():
     root = tkinter.Tk()
@@ -40,13 +79,16 @@ def main():
     #s = ttk.Style()
     #s.theme_use('clam')
     #s.configure('.', font=f)
-    root.title('CodeMagic V0.1 Build20160830')
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-    root.resizable(True, False)
     app = Application(master=root)
+    app.master.title('CodeMagic V0.1 Build20160830')
+    app.master.columnconfigure(0, weight=1)
+    app.master.rowconfigure(0, weight=1)
+    # app.master.maxsize(1000, 4000)
+    app.master.resizable(True, False)
     app.mainloop()
 
-# -----------------------------------------
+
+#End main function------------------------------------------------------------------------------------------------------
+
 if __name__ == '__main__':
     main()
